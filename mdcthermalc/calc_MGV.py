bcda905ccebd61ce03fb9ac8104ba50bae91908b
Tao Fan <48915805+Baijianlu@users.noreply.github.com>
2019-03-29 17:37:58 +0300

    Update calc_MGV.py

--- mdcthermalc/calc_MGV.py
+++ mdcthermalc/calc_MGV.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 17 12:43:43 2019
+Copyright (C) 2019 Tao Fan 
+All rights reserved
 
-@author: Tao.Fan
-This script for calculating mode group velocity by averaging points around Gamma points
+This script is used for calculating group velocity, Debye temperature of each branch. The group velocity of branch is weighted average value of each path 
+in this branch. Debye temperature of each branch is calculated by using the highest frequency of the branch. 
+
 """
 import numpy as np
 import scipy.constants
@@ -14,27 +16,24 @@ def calc_MGV(filepath,weight):
     Boltzm = scipy.constants.Boltzmann
     
     (GroupVec,Frequency,Gamma) = extract_GV(filepath)
-#    Gamma = np.array([0.1363090,0.5483771,0.9786495])              #Gamma point should be decided by external tools   Mg2Si:0.1363090,0.5483771   LiMg[0.14588740,0.75077500]   AL3Li[0.12431940,0.63978040]
-    Gamma_index = np.zeros((len(Gamma),2))          #Gamma point maybe the first one, thus the index could be zero, must be careful
+    Gamma_index = np.zeros((len(Gamma),2))          
     Bandnum = GroupVec.shape[1] - 1
     Minindex = np.int(0)
     Maxindex = np.int(GroupVec.shape[0] - 1)
-#    weight = np.array([8,12,48,24,24])                  #this should be decided by function   Mg2Si:[8,6,24,12]   LiMg:[6,12,8] Al3Li:[6,12,8]
-#    (no1,no2,weight) = get_highsympath("Mg2Si_mp-1367.cif")
-    #搜索groupvec得到Gamma点的位置，并向gamma点左右两边各取5个点
+
+    #search groupvec to get Gamma positions
     Pathnum = 0
     for i in np.arange(len(Gamma)):
-        Gamma_index[i] = np.array([x_index for x_index,x_value in enumerate(GroupVec[:,0]) if x_value==Gamma[i]])
-    ##此处需要有个语句来判断有多少条路径出现，从而确定modebranch_vel的第二维的大小    
+        Gamma_index[i] = np.array([x_index for x_index,x_value in enumerate(GroupVec[:,0]) if x_value==Gamma[i]])   
         if Gamma_index[i,0] == Gamma_index[i,1]:Pathnum = Pathnum + 1
         else: Pathnum = Pathnum + 2
     
-    ####
+    #the following is for calculating average group velocity of different branch
     modebranch_vel = np.zeros((Bandnum,Pathnum,5))   #the first dimension size equal natom*3
     branch_vel = np.zeros(Bandnum)
     
     for branch_idx in np.arange(Bandnum):
-        for j in np.arange(len(Gamma)):                 #此处j的范围是非对称路径的条数
+        for j in np.arange(len(Gamma)):                 #
             for k in np.arange(2):
                 if k == 0: 
                     if Gamma_index[j,k] > Minindex:
@@ -53,9 +52,7 @@ def calc_MGV(filepath,weight):
             branch_vel[branch_idx] = branch_vel[branch_idx] + weight[j] * np.average(modebranch_vel[branch_idx,j,:])
         branch_vel[branch_idx] = branch_vel[branch_idx] / np.sum(weight)
     
-#    [np.savetxt('modevelocity' + np.str(i) + '.out',modebranch_vel[i],fmt='%.8f',delimiter=',') for i in np.arange(Bandnum)]
-    
-    ###the following is for calculating average frequency for different branch
+    #the following is for calculating average frequency of different branch
     modebranch_freq = np.zeros((Bandnum,Pathnum,51))
     branch_freq = np.zeros(Bandnum)
     
@@ -78,7 +75,7 @@ def calc_MGV(filepath,weight):
             branch_freq[branch_idx] = branch_freq[branch_idx] + weight[j] * np.average(modebranch_freq[branch_idx,j,:])
         branch_freq[branch_idx] = branch_freq[branch_idx] / np.sum(weight)    
     
-    ###the following is for calculating debye temperature for different branch
+    #the following is for calculating debye temperature for different branch
     branch_DebyeT = np.zeros(Bandnum)
     for branch_idx in np.arange(Bandnum):
         branch_DebyeT[branch_idx] = planck * np.max(Frequency[:,branch_idx+1]) * 1e12/Boltzm
